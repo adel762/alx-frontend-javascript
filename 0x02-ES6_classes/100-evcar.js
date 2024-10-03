0987ccc3502774698e424bb7602b8a2343b11f19
Mohamed_Adel <adel762dp47@gmail.com>
2024-10-03 15:58:34 -0700

    message

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,18 +1,14 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    // Call the parent class constructor
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // Override the cloneCar method
-  cloneCar() {
-    // Return a new instance of Car with the original brand, motor, and color
-    return new Car(this._brand, this._motor, this._color);
+  static get [Symbol.species]() {
+    return Car;
   }
 }
 
 export default EVCar;
-
