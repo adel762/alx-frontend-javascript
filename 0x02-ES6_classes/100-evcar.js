9716c5e9d2b4013f9a99c8bcc7fb5fb18d32faa6
Mohamed_Adel <adel762dp47@gmail.com>
2024-10-03 15:30:21 -0700

    message

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from ;
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
